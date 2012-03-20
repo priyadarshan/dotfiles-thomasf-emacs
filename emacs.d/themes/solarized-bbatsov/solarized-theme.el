079b0b21ef07a7e5bee5c404e6a4bd48a48c3e86
Thomas Frössman <thomasf@jossystem.se>
2012-03-20 13:56:40 +0100

    Themes updates

--- emacs.d/themes/solarized-bbatsov/solarized-theme.el
+++ emacs.d/themes/solarized-bbatsov/solarized-theme.el
@@ -4,7 +4,7 @@
 
 ;; Author: Bozhidar Batsov <bozhidar.batsov@gmail.com>
 ;; URL: http://github.com/bbatsov/solarized-emacs
-;; Version: 0.2.1
+;; Version: 0.3.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -443,12 +443,15 @@
      `(wl-highlight-summary-displaying-face ((,class (:underline t :weight bold))))
 
      ;; which-func-mode
-     `(which-func ((,class (:foreground ,green))))))
+     `(which-func ((,class (:foreground ,green)))))
 
-  (custom-theme-set-variables
-   (if (eq variant 'light) 'solarized-light 'solarized-dark)
-   '(ansi-color-names-vector [solarized-bg red green yellow
-                                           blue magenta cyan solarized-fg])))
+    (custom-theme-set-variables
+     (if (eq variant 'light) 'solarized-light 'solarized-dark)
+     '(ansi-color-names-vector [solarized-bg red green yellow
+                                             blue magenta cyan solarized-fg])
+
+     ;; fill-column-indicator
+     `(fci-rule-color ,solarized-hl))))
 
 ;;;###autoload
 (when load-file-name
