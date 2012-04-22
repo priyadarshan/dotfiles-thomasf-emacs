b37c7fe06c2fda585421323532ee9c5df933a2cf
Thomas Frössman <thomasf@jossystem.se>
2012-04-22 03:51:41 +0200

    Solarized child theme

---
+++ emacs.d/themes/thomasf-solarized-dark-theme.el
@@ -0,0 +1,7 @@
+(require 'solarized-theme)
+
+(deftheme thomasf-solarized-dark "The dark variant of the Solarized colour theme")
+
+(create-solarized-theme 'dark 'thomasf-solarized-dark 'thomasf-solarized-childtheme)
+
+(provide-theme 'thomasf-solarized-dark)
---
+++ emacs.d/themes/thomasf-solarized-light-theme.el
@@ -0,0 +1,7 @@
+(require 'solarized-theme)
+
+(deftheme thomasf-solarized-light "The light variant of the Solarized colour theme")
+
+(create-solarized-theme 'light 'thomasf-solarized-light 'thomasf-solarized-childtheme)
+
+(provide-theme 'thomasf-solarized-light)
