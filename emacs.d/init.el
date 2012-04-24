2ead4b87e5fb23e0e3cbd19631f76ccd771bb997
Thomas Frössman <thomasf@jossystem.se>
2012-04-24 21:13:46 +0200

    Groundwork for a future.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,7 +1,7 @@
 (setq dotfiles-dir 
       (file-name-directory (or load-file-name (buffer-file-name))))
 (add-to-list 'load-path dotfiles-dir)
-(require 'thomasf-init)
+(require 'thomasf-bootstrap)
 
 ;; Load main configuration
 (org-babel-load-file (expand-file-name "thomasf-emacs.org" dotfiles-dir))
--- emacs.d/thomasf-bootstrap.el
+++ emacs.d/thomasf-bootstrap.el
@@ -4,7 +4,9 @@
 ;; This file is generated from thomasf-emacs.org
 ;;
 
-(provide 'thomasf-init)
+(provide 'thomasf-bootstrap)
+
+(setq max-lisp-eval-depth 1000)
 
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
