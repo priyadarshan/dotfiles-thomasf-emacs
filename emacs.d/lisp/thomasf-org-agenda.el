a1239a1ef840ce6238582a3d00219601c4ed6d5d
Thomas Frössman <thomasf@jossystem.se>
2012-04-27 08:44:22 +0200

    Missed a org-agenda skip function + changed flymake loading

--- emacs.d/lisp/thomasf-org-agenda.el
+++ emacs.d/lisp/thomasf-org-agenda.el
@@ -210,6 +210,26 @@
               nil)) ; a stuck project, has subtasks but no next task
         next-headline))))
 
+(defun bh/skip-non-archivable-tasks ()
+  "Skip trees that are not available for archiving"
+  (save-restriction
+    (widen)
+    (let ((next-headline (save-excursion (or (outline-next-heading) (point-max)))))
+      ;; Consider only tasks with done todo headings as archivable candidates
+      (if (member (org-get-todo-state) org-done-keywords)
+          (let* ((subtree-end (save-excursion (org-end-of-subtree t)))
+                 (daynr (string-to-int (format-time-string "%d" (current-time))))
+                 (a-month-ago (* 60 60 24 (+ daynr 1)))
+                 (last-month (format-time-string "%Y-%m-" (time-subtract (current-time) (seconds-to-time a-month-ago))))
+                 (this-month (format-time-string "%Y-%m-" (current-time)))
+                 (subtree-is-current (save-excursion
+                                       (forward-line 1)
+                                       (and (< (point) subtree-end)
+                                            (re-search-forward (concat last-month "\\|" this-month) subtree-end t)))))
+            (if subtree-is-current
+                next-headline ; Has a date in this month or last month, skip it
+              nil))  ; available to archive
+        (or next-headline (point-max))))))
 
 ;; functions for projects
 
@@ -281,8 +301,6 @@
       (setq org-tags-match-list-sublevels t)
     (setq org-tags-match-list-sublevels nil))
   nil)
-
-
 
 (defun bh/find-project-task ()
   "Move point to the parent (project) task if any"
